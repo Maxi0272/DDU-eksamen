bcf4d7d127f77ed6039e148e7d6ccac66195958d
Maxi0272 <maximilian.b.shf@gmail.com>
2024-05-19 04:26:50 +0200

    added popup with info
    added popup window with control information

--- Program/Box_list_container.gd
+++ Program/Box_list_container.gd
@@ -1,15 +1,1 @@
 extends VBoxContainer
-const inner_box = preload("res://Inner_box_template.tscn")
-
-
-func _on_add_new_pressed():
-#	await get_tree().create_timer(0.5).timeout
-#	var box_list_inst = inner_box.instantiate()
-#	self.add_child(box_list_inst)
-	pass
-
-
-func _on_new_box_buttonpress():
-#	var box_list_inst = inner_box.instantiate()
-#	self.add_child(box_list_inst)
-	pass
--- Program/main.gd
+++ Program/main.gd
@@ -1,17 +1,14 @@
 extends Node3D
 @onready var new_box = $Canvas_layer/HUD/New_box
 const inner_box = preload("res://Inner_box_template.tscn")
-@onready var box_list_container = $Canvas_layer/HUD/Box_list_container
+
 
 func _on_add_new_pressed():
 	new_box.show()
 	if Manager.shown == 0:
-		print ("working")
 		var new_dict = {Manager.id:{"id" : Manager.id, "name" : "", "color" : Color(0,0,0,1),  "freq_low" : 50, "freq_high" : 50, "pan_pos" : 50, "pan_size" : 50 ,"depth_low" : 50, "depth_high": 50, "hidden" : false, "editing" : false}}
 		Manager.test.merge(new_dict)
 		Manager.shown = 1
 	var box_3d = load("res://standard_box.tscn").instantiate()
 	$Outerbox.add_child(box_3d)
 	box_3d.position = Vector3(-50,-50,-50)
-#	var box_list_inst = inner_box.instantiate()
-#	box_list_container.add_child(box_list_inst)
